f9bd562d77a7ab97d76f944000b1ebbc0c7deb82
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2025-06-03 13:57:46 +0100

    Add initial tests for daily_max and min

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,16 +2,15 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_max, daily_mean, daily_min
+
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
-    
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
+    test_input = np.array([[0, 0], [0, 0], [0, 0]])
     test_result = np.array([0, 0])
 
     # Need to use Numpy testing functions to compare arrays
@@ -21,11 +20,45 @@ def test_daily_mean_zeros():
 def test_daily_mean_integers():
     """Test that mean function works for an array of positive integers."""
 
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
+    test_input = np.array([[1, 2], [3, 4], [5, 6]])
     test_result = np.array([3, 4])
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_daily_max_positive_int():
+    """Test that max function works for an array of positive integers."""
+    test_input = np.array([[4, 2, 5], [1, 6, 2], [4, 1, 9]])
+
+    test_result = np.array([4, 6, 9])
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_max_negative_int():
+    """Test that max function works for an array containing negatives."""
+    test_input = np.array([[-4, -2], [-1, 6], [4, -9]])
+
+    test_result = np.array([4, 6])
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_max_zeros():
+    """Test that max function works for an array of zeros."""
+    test_input = np.array([[0], [0], [0]])
+
+    test_result = np.array([0])
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min():
+    """Test that min function works for an array of positive and negative integers."""
+    test_input = np.array([[4, -2, 5], [1, -6, 2], [-4, -1, 9]])
+    test_result = np.array([-4, -6, 2])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+def test_daily_min_error_string():
+    with pytest.raises(TypeError):
+        daily_min([["Hello", "There"], ["General", "Kenobi"]])
