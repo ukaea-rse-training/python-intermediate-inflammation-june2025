fe80b44b8643a3da9c9466c9a3a2d8d2beca333d
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2025-06-03 14:28:31 +0100

    Parametrize daily_max tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -27,28 +27,20 @@ def test_daily_mean_integers():
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
 
-def test_daily_max_positive_int():
+@pytest.mark.parametrize(
+    ("test", "expected"),
+    [
+        [[[4, 2, 5], [1, 6, 2], [4, 1, 9]], [4, 6, 9]],
+        [[[-4, -2], [-1, 6], [4, -9]], [4, 6]],
+        [[[0], [0], [0]], [0]],
+    ],
+    ids=["positive ints", "negative ints", "zeros"],
+)
+def test_daily_max(test, expected):
     """Test that max function works for an array of positive integers."""
-    test_input = np.array([[4, 2, 5], [1, 6, 2], [4, 1, 9]])
+    test_input = np.array(test)
 
-    test_result = np.array([4, 6, 9])
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-
-def test_daily_max_negative_int():
-    """Test that max function works for an array containing negatives."""
-    test_input = np.array([[-4, -2], [-1, 6], [4, -9]])
-
-    test_result = np.array([4, 6])
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-
-def test_daily_max_zeros():
-    """Test that max function works for an array of zeros."""
-    test_input = np.array([[0], [0], [0]])
-
-    test_result = np.array([0])
-    npt.assert_array_equal(daily_max(test_input), test_result)
+    npt.assert_array_equal(daily_max(test_input), expected)
 
 
 def test_daily_min():
@@ -59,6 +51,13 @@ def test_daily_min():
     npt.assert_array_equal(daily_min(test_input), test_result)
 
 
-def test_daily_min_error_string():
-    with pytest.raises(TypeError):
-        daily_min([["Hello", "There"], ["General", "Kenobi"]])
+@pytest.mark.parametrize(
+    "test",
+    [
+        [["Hello", "There"], ["General", "Kenobi"]],
+        [["Hello", None], [1.2, []]],
+    ],
+)
+def test_daily_min_error_string(test):
+    with pytest.raises(Exception):
+        daily_min(test)
