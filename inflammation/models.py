18246f87c50c080ebb312a803e96f7b036d94db9
bielsnohr <6177028+bielsnohr@users.noreply.github.com>
2025-06-03 16:48:32 +0100

    Add patient_normalise function to models

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
@@ -15,7 +15,7 @@ def load_csv(filename):
 
     :param filename: Filename of CSV to load
     """
-    return np.loadtxt(fname=filename, delimiter=',')
+    return np.loadtxt(fname=filename, delimiter=",")
 
 
 def daily_mean(data):
@@ -31,3 +31,9 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
+
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    max = np.max(data, axis=1)
+    return data / max[:, np.newaxis]
--- tests/test_models.py
+++ tests/test_models.py
@@ -4,7 +4,7 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
-from inflammation.models import daily_max, daily_mean, daily_min
+from inflammation.models import daily_max, daily_mean, daily_min, patient_normalise
 
 
 def test_daily_mean_zeros():
@@ -67,3 +67,20 @@ def test_daily_min(test, expected):
 def test_daily_string_raises_error():
     with pytest.raises(TypeError):
         error_expected = daily_min([["Hello", "there"], ["General", "Kenobi"]])
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+        )
+    ],
+)
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers.
+    Test with a relative and absolute tolerance of 0.01."""
+
+    result = patient_normalise(np.array(test))
+    npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
